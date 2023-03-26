ef4397a84ffe2af8c30595b6dcd03ba887e52e3f
AleksandrZhukovin <sashokzuk@gmail.com>
2023-03-26 21:17:06 +0300

    add(model): user's profile model creation

--- instagramapp/models/profile.py
+++ instagramapp/models/profile.py
@@ -1,3 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=200)
+    location = models.CharField(max_length=30, default=' ')
+    avatar = models.ImageField(upload_to='static/avatars')
