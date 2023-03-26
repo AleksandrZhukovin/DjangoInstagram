fbc0265a158572c61adf414ab82c5bbc0d609b46
AleksandrZhukovin <sashokzuk@gmail.com>
2023-03-26 21:24:37 +0300

    update(abstract model): add image field

--- instagramapp/models/abstract.py
+++ instagramapp/models/abstract.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Abstract(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='static/images')
 
     class Meta:
         abstract = True
--- instagramapp/models/profile.py
+++ instagramapp/models/profile.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from abstract import Abstract
 
 
-class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Profile(Abstract):
     status = models.CharField(max_length=200)
     location = models.CharField(max_length=30, default=' ')
-    avatar = models.ImageField(upload_to='static/avatars')
