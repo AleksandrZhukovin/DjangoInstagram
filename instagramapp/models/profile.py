eee1603ae6c3465082a707b974b56d33be7bead5
AleksandrZhukovin <sashokzuk@gmail.com>
2023-03-29 21:56:00 +0300

    fix(models): fix profile model`s follow fields with related_name

--- instagramapp/models/profile.py
+++ instagramapp/models/profile.py
@@ -1,7 +1,10 @@
 from django.db import models
 from .abstract import Abstract
+from django.contrib.auth.models import User
 
 
 class Profile(Abstract):
     status = models.CharField(max_length=200, default=' ')
     location = models.CharField(max_length=30, default=' ')
+    following = models.ManyToManyField(User, related_name='following')
+    followers = models.ManyToManyField(User, related_name='followers')
