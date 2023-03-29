59b14ea5d0240fbdde31e55d4270a327e3ce83ba
AleksandrZhukovin <sashokzuk@gmail.com>
2023-03-29 22:05:17 +0300

    fix(models): add fields to comment, migrate like model

--- instagramapp/models/__init__.py
+++ instagramapp/models/__init__.py
@@ -1,3 +1,5 @@
 from .post import Post
 from .profile import Profile
+from .comments import Comment
+from .like import Like
 from django.contrib.auth.models import User
--- instagramapp/models/comments.py
+++ instagramapp/models/comments.py
@@ -6,3 +6,5 @@ from .post import Post
 class Comment(models.Model):
     body = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
