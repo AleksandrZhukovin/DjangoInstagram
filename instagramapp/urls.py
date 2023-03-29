b26eeb6acc0770cebc7ffcd56c2be3c7253b7ae0
AleksandrZhukovin <sashokzuk@gmail.com>
2023-03-29 21:58:34 +0300

    fix(views): fix follow, unfollow, profile, search views

--- instagramapp/urls.py
+++ instagramapp/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('profile/', login_required(views.ProfileView.as_view()), name='profile'),
     path('add_post/', login_required(views.AddPostView.as_view()), name='add_post'),
     path('search/', views.SearchView.as_view(), name='search'),
-    path('search/<str:search>/', views.SearchResultView.as_view(), name='search_res')
+    path('search/<str:search>/', views.SearchResultView.as_view(), name='search_res'),
+    path('user_profile<int:pk>/', views.UserProfileView.as_view(), name='user_profile'),
+    path('follow<int:pk>/', views.FollowView.as_view(), name='follow'),
+    path('unfollow<int:pk>/', views.UnfollowView.as_view(), name='unfollow'),
+    path('add_post/edit_post<int:pk>/', views.EditPostView.as_view(), name='edit_post')
 ]
--- instagramapp/views.py
+++ instagramapp/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 from .models import Post, Profile
 from .forms import RegistrationForm, ProfileEdit, SearchForm
 from django.views.generic.edit import CreateView, UpdateView, FormView
@@ -16,6 +15,8 @@ class ProfileView(TemplateView):
     def get_context_data(self, **kwargs):
         user = self.request.user
         context = super().get_context_data(**kwargs)
+        if user.is_authenticated:
+            profile, created = Profile.objects.get_or_create(user=user)
         context['title'] = _('Profile {user}').format(user=user.username)
         context['user'] = user
         context['posts'] = Post.objects.filter(user=user)
@@ -32,11 +33,6 @@ class RegistrationView(CreateView):
         context = super().get_context_data(**kwargs)
         context['title'] = _('Sign Up')
         return context
-
-    def form_valid(self, form):
-        profile = Profile(user=self.request.user)
-        profile.save()
-        return super().form_valid(form)
 
 
 class Login(LoginView):
@@ -76,6 +72,7 @@ class EditPostView(UpdateView):
     model = Post
     template_name = 'add_post.html'
     fields = ['image', 'description']
+    success_url = reverse_lazy('home')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -120,7 +117,44 @@ class UserProfileView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        current_user = self.request.user
+        current_user_profile = Profile.objects.get(user=current_user)
         user = User.objects.get(id=self.kwargs['pk'])
+        context['follow'] = False
+        if user not in current_user_profile.following.all():
+            context['follow'] = True
         context['title'] = user.username
         context['profile'] = Profile.objects.get(user=user)
+        context['followers'] = len(Profile.objects.get(user=user).followers.all())
+        context['following'] = len(Profile.objects.get(user=user).following.all())
+        context['user'] = current_user
+        context['posts'] = Post.objects.filter(user=user)
         return context
+
+
+class FollowView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        current_user = self.request.user
+        pk = self.kwargs['pk']
+        current_user_profile = Profile.objects.get(user=current_user)
+        current_user_profile.following.add(User.objects.get(id=pk))
+        current_user_profile.save()
+        f_user = Profile.objects.get(user=User.objects.get(id=pk))
+        f_user.followers.add(current_user)
+        f_user.save()
+        self.url = '/user_profile{0}/'.format(pk)
+        return super().get_redirect_url(*args, **kwargs)
+
+
+class UnfollowView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        current_user = self.request.user
+        pk = self.kwargs['pk']
+        current_user_profile = Profile.objects.get(user=current_user)
+        current_user_profile.following.remove(User.objects.get(id=pk))
+        current_user_profile.save()
+        f_user = Profile.objects.get(user=User.objects.get(id=pk))
+        f_user.followers.remove(current_user)
+        f_user.save()
+        self.url = '/user_profile{0}/'.format(pk)
+        return super().get_redirect_url(*args, **kwargs)
