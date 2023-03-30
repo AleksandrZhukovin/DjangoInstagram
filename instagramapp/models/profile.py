b608352977fba568552a182749bf24e828e86672
AleksandrZhukovin <sashokzuk@gmail.com>
2023-03-30 21:54:27 +0300

    fix(model): fix abstract model by setting null and blank params

--- instagramapp/models/profile.py
+++ instagramapp/models/profile.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Profile(Abstract):
-    status = models.CharField(max_length=200, default=' ')
-    location = models.CharField(max_length=30, default=' ')
+    status = models.CharField(max_length=200, default=' ', null=True, blank=True)
+    location = models.CharField(max_length=30, default=' ', null=True, blank=True)
     following = models.ManyToManyField(User, related_name='following')
     followers = models.ManyToManyField(User, related_name='followers')
