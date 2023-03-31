baa6024b2843d21bd907a9859592b71829149f6d
AleksandrZhukovin <sashokzuk@gmail.com>
2023-03-31 22:43:36 +0300

    fix(model): add comment field to like model, add null params

--- instagramapp/models/like.py
+++ instagramapp/models/like.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from .post import Post
+from .comments import Comment
 
 
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True)
