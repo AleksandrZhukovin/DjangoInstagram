f94d004bf6f11dbc80bde80a49e7c6ee8a7b86c2
AleksandrZhukovin <sashokzuk@gmail.com>
2023-06-10 23:27:37 +0300

    fix(urls): include url files from all apps

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('instagramapp.urls')),
+    path('', include('chat.urls')),
+    path('', include('authentication.urls')),
+    path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
