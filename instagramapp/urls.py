ee90624a495c1fa0a1a6c6049047dc075bbc4320
AleksandrZhukovin <sashokzuk@gmail.com>
2023-06-10 23:25:04 +0300

    fix(url): fix add_post url

--- instagramapp/urls.py
+++ instagramapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', login_required(views.HomeView.as_view()), name='home'),
     path('search/', views.SearchView.as_view(), name='search'),
+    path('add_post/', views.AddPostView.as_view(), name='add_post'),
     path('post/<int:pk>/', views.PostView.as_view(), name='post_view')
 ]
