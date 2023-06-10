e942e7d4db939a2b270b8551ec87307264f76d24
AleksandrZhukovin <sashokzuk@gmail.com>
2023-06-10 23:25:57 +0300

    fix(model): fix post model with changing files upload path

--- instagramapp/models/post.py
+++ instagramapp/models/post.py
@@ -4,4 +4,4 @@ from .abstract import Abstract
 
 class Post(Abstract):
     description = models.CharField(max_length=1000, default='')
-    image = models.ImageField(upload_to='instagramapp/static/images/')
+    image = models.ImageField(upload_to='static/images/')
