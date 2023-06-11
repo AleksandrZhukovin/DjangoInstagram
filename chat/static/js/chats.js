529be5d1179fa7ec85e7734229cfba3fb244bcf1
AleksandrZhukovin <sashokzuk@gmail.com>
2023-06-11 12:14:44 +0300

    fix(js): fix chat.js with correct elements classes and aditional conditions

--- chat/static/js/chats.js
+++ chat/static/js/chats.js
@@ -2,33 +2,39 @@ $(function(){
     $(document).click(function(event) {
     b_id = $(event.target);
     console.log(b_id);
-    if (b_id.attr('class') == 'profile-image pr-3' || b_id.attr('class') == null || b_id.attr('class') == 'profile_link mx-2'){
-    $.ajax(b_id.data('url'), {
-        'type': 'POST',
-        'async': true,
-        'dataType': 'json',
-        'data': {
-            'chat': b_id.data('id'),
-            'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val()
-        },
-        'success': function(data){
-            document.getElementById('chat').innerHTML += data;
-        }
-        })}
-        else if (b_id.attr('id') == 'input'){
+    if (b_id.attr('class') == 'profile-image pr-3' || b_id.attr('class') == null || b_id.attr('class') == 'btn_snd open mx-2'){
+        if (document.getElementById('chat').innerHTML == '') {
             $.ajax(b_id.data('url'), {
                 'type': 'POST',
                 'async': true,
                 'dataType': 'json',
                 'data': {
-                    'message': document.getElementById('id_body').value,
                     'chat': b_id.data('id'),
                     'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val()
                 },
                 'success': function(data){
-                    document.getElementById('messages').innerHTML += '<p class="u_msg">' + data['message'] + '</p><br>';
-                    document.getElementById('id_body').value = '';
+                    document.getElementById('chat').innerHTML += data;
                 }
             })
+        } else {
+            document.getElementById('chat').innerHTML = '';
         }
+
+
+    }else if (b_id.attr('id') == 'input'){
+        $.ajax(b_id.data('url'), {
+            'type': 'POST',
+            'async': true,
+            'dataType': 'json',
+            'data': {
+                'message': document.getElementById('id_body').value,
+                'chat': b_id.data('id'),
+                'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val()
+            },
+            'success': function(data){
+                document.getElementById('messages').innerHTML += '<div class="row"><div> <p class="u_msg float-end">' + data['message'] + '</p><br></div></div>';
+                document.getElementById('id_body').value = '';
+            }
+        })
+    }
     })})
