e700223dc041eb30b7db18edfefeff194de20054
AleksandrZhukovin <sashokzuk@gmail.com>
2023-06-12 14:26:16 +0300

    fix(settings): fix staticfiles_dir list, remove static_root

--- instagram/settings.py
+++ instagram/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'instagramapp/static'),
     os.path.join(BASE_DIR, 'chat/static'),
     os.path.join(BASE_DIR, 'authentication/static'),
