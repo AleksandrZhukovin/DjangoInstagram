254ce9032c71bf01b33754d1c06eff8ddb65d852
AleksandrZhukovin <sashokzuk@gmail.com>
2023-06-18 22:53:24 +0300

    fix(files): add media dir for images storage

--- instagram/settings.py
+++ instagram/settings.py
@@ -135,6 +135,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'accounts/static')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
